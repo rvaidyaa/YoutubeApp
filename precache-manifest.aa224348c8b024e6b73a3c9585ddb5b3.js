d43715c7278a9a598cba5c08d2bc361a24ab882c
rvaidyaa <rvaidyaa@gmail.com>
2018-10-29 15:29:28 -0700

    Updates

---
+++ precache-manifest.aa224348c8b024e6b73a3c9585ddb5b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a5acec592873b834e85",
+    "url": "/YoutubeApp/static/js/runtime~main.1a5acec5.js"
+  },
+  {
+    "revision": "c01714467c72f62cd1eb",
+    "url": "/YoutubeApp/static/js/main.c0171446.chunk.js"
+  },
+  {
+    "revision": "fb559a67b2082578fe03",
+    "url": "/YoutubeApp/static/js/1.fb559a67.chunk.js"
+  },
+  {
+    "revision": "c01714467c72f62cd1eb",
+    "url": "/YoutubeApp/static/css/main.ac47c4ef.chunk.css"
+  },
+  {
+    "revision": "fb559a67b2082578fe03",
+    "url": "/YoutubeApp/static/css/1.00000649.chunk.css"
+  },
+  {
+    "revision": "8eafb59d7615cafc3ce29f9bfec8c279",
+    "url": "/YoutubeApp/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/YoutubeApp/precache-manifest.aa224348c8b024e6b73a3c9585ddb5b3.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/YoutubeApp/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
